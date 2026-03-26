2406b1536e7e7d8d85c944b3e0c00e1eca271a5a
MitsukiHaruko <MitsukiHaruko1999@gmail.com>
2026-03-26 13:15:14 +0100

    Update GAZELL3D.user.js

--- GAZELL3D.user.js
+++ GAZELL3D.user.js
@@ -5,8 +5,6 @@
 // @description  Reimagine UNIT3D-based torrent pages for readability with a two-column layout, richer metadata presentation, cleaner torrent naming, and minor quality-of-life tweaks.
 // @match        https://aither.cc/torrents/*
 // @match        https://aither.cc/torrents*
-// @updateURL    https://github.com/anonymoize/GAZELL3D/raw/refs/heads/main/GAZELL3D.user.js
-// @downloadURL  https://github.com/anonymoize/GAZELL3D/raw/refs/heads/main/GAZELL3D.user.js
 // @grant        GM_addStyle
 // @grant        GM_xmlhttpRequest
 // @grant        GM_setValue
